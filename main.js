ef8fdca8478f5bc0853a88b26127f082ec43eef7
Jacob Sides <sides798@gmail.com>
2022-10-22 17:54:49 -0400

    sdk api example

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-console.log('Here')
+const sdk = require('api')('@fsq-docs/v1.0#psm3fl8yqdb71');
 
-
+sdk.auth('fsq3w2Qi4keLkoD1BrCJepmazrZksXsfNvRrDc0QBgCejJM=');
+sdk.placeSearch()
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
