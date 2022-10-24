91b7b86bcb0d6f2d9aeac2f72ec39ac19c497dc4
Jacob Sides <sides798@gmail.com>
2022-10-24 10:49:57 -0400

    AllowCrossDomain

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const express = require("express");
 // Creating express app object
 const app = express();
 
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+app.use(allowCrossDomain);
+
 // Handling '/' route
 app.get("/", async (req, res) => {
     const data = await main(req.query)
